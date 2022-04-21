932e0548752c202d033be672c52eb2310eded905
Abrar051 <shahriar1410051@gmail.com>
2022-04-21 13:40:37 -0700

    Add project files.

---
+++ CrudWebAppSample/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudWebAppSample.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public Profile()
+        {
+
+        }
+    }
+}
